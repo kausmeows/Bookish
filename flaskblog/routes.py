b68e11b6b633dedd991ebe9e286b5d9d3d848141
Kaustubh Shukla <kaustubh@Kaustubhs-MacBook-Air.local>
2022-01-12 01:52:17 +0530

    added the pagination funcationality.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -12,7 +12,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")       # 2 routes are handled by the same function. 
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=4)
     return render_template('home.html', posts = posts)
 
 @app.route("/about")
@@ -142,3 +143,12 @@ def delete_post(post_id):
     return redirect(url_for('home'))
 
 
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404() # get the first user with this username and if not found return error 404
+    posts = Post.query.filter_by(author=user)\
+             .order_by(Post.date_posted.desc())\
+             .paginate(page=page, per_page=4)
+    return render_template('user_post.html', posts=posts, user=user)
