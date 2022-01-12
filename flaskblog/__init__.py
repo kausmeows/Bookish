03b2a41b26142cc31faeef6bdad8f4b52f21b136
Kaustubh Shukla <kaustubh@Kaustubhs-MacBook-Air.local>
2022-01-12 21:44:41 +0530

    Reset password via email feature added.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_manager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a22912297e93845c6cf4776991df63ec'
@@ -10,5 +12,15 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "noreplya006@gmail.com"
+app.config['MAIL_PASSWORD'] = "#@1234abcd"
+mail = Mail(app)
+
+mail = Mail(app)
 
 from flaskblog import routes
